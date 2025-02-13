185672ae3877e84eafaa3eebc73aa4b17c696dc7
Mario2309 <151546364+Mario2309@users.noreply.github.com>
2025-02-13 18:13:08 +0100

    initial commit

---
+++ menu/menu_principal.gd
@@ -0,0 +1,9 @@
+extends Control
+
+
+func _on_btn_start_pressed():
+	get_tree().change_scene_to_file("res://environment/environment.tscn")
+
+
+func _on_btn_exit_pressed() -> void:
+	get_tree().quit()
